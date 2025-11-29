998e0fd0bf7a293e4afa7924f670fe747c5f7506
Damithu04 <94391642+Damithu04@users.noreply.github.com>
2025-11-29 23:48:05 +0530

    javascript for home page

--- Webtech.js
+++ Webtech.js
@@ -20,3 +20,13 @@ function submitForm() {
         closePopup();
     }
 }
+
+   // Optional: Smooth scrolling effect for anchor links
+   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
